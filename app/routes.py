157a04cf54dd3619e13b0c230896a07bd46f9dad
whxcer <whxcer@outlook.com>
2020-11-01 17:34:21 +0800

    添加数据库模型类 and 保存数据到数据库 and 检索数据并显示在页面上

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,20 @@
-from app import app
+from app import app, db
 from flask import render_template, flash, redirect, url_for
 from app.forms import HelloWorldForm
+from app.models import Message
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     title = 'Hello World'
     form = HelloWorldForm()
     if form.validate_on_submit():
+        name = form.name.data
+        body = form.message.data
+        message = Message(name=name, body=body)
+        db.session.add(message)
+        db.session.commit()
         flash('Your message have been sent to the world!')
         return redirect(url_for('index'))
-    return render_template('index.html', title=title, form=form)
+
+    messages = Message.query.order_by(Message.timestamp.desc()).all()
+    return render_template('index.html', title=title, form=form, messages=messages)
--- config.py
+++ config.py
@@ -1,4 +1,18 @@
 import os
+import sys
+
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app_db = prefix + os.path.join(basedir, 'app.db')
+
 
 class Config():
     SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', app_db)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
