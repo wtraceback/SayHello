c746fa08ca9d268f669bf8471446d037bc5f55f0
whxcer <whxcer@outlook.com>
2020-11-01 12:09:11 +0800

    接收表单数据 and 对字段数据进行校验并显示验证失败信息 and Flash消息

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app.forms import HelloWorldForm
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     title = 'Hello World'
     form = HelloWorldForm()
+    if form.validate_on_submit():
+        flash('Your message have been sent to the world!')
+        return redirect(url_for('index'))
     return render_template('index.html', title=title, form=form)
